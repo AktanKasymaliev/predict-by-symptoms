806eb6f908aab33e3770f7ea1809b7c46590ada9
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2023-05-07 00:41:09 +0600

    changed static folders path

--- webapp/config/settings.py
+++ webapp/config/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'frontend/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/staticfiles')
+STATIC_URL = '/frontend/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend', 'static'),
 )
 
 # Default primary key field type
